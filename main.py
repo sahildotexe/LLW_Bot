9e2c62615ecae7efe0ae69c3cd4c46b7fe156e7c
kashish0603 <kashish.jain2021a@vitstudent.ac.in>
2023-12-27 11:53:45 +0530

    main file updated

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     import uvicorn
 
     # Run the FastAPI app using UVicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
